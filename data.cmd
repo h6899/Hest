01be09d769ce296af45f60736984210ed717b750
h6899 <draven6@live.no>
2016-08-14 23:55:02 +0200

    Better Text

--- data.cmd
+++ data.cmd
@@ -52,13 +52,13 @@ echo 7. Hest 7
 echo 8. Avbryt
 echo.
 set /p start=Velg:
-if "%start%" == "1" set v=1 & goto v
-if "%start%" == "2" set v=2 & goto v
-if "%start%" == "3" set v=3 & goto v
-if "%start%" == "4" set v=4 & goto v
-if "%start%" == "5" set v=5 & goto v
-if "%start%" == "6" set v=6 & goto v
-if "%start%" == "7" set v=7 & goto v
+if "%start%" == "1" set v=1 & set tall=insertbmp /p:"tall1.bmp" /x:125 /y:624 & goto v
+if "%start%" == "2" set v=2 & set tall=insertbmp /p:"tall2.bmp" /x:125 /y:624 & goto v
+if "%start%" == "3" set v=3 & set tall=insertbmp /p:"tall3.bmp" /x:125 /y:624 & goto v
+if "%start%" == "4" set v=4 & set tall=insertbmp /p:"tall4.bmp" /x:125 /y:624 & goto v
+if "%start%" == "5" set v=5 & set tall=insertbmp /p:"tall5.bmp" /x:125 /y:624 & goto v
+if "%start%" == "6" set v=6 & set tall=insertbmp /p:"tall6.bmp" /x:125 /y:624 & goto v
+if "%start%" == "7" set v=7 & set tall=insertbmp /p:"tall7.bmp" /x:125 /y:624 & goto v
 if "%start%" == "8" goto meny
 goto start
 
@@ -73,8 +73,8 @@ insertbmp /p:"back2.bmp" /x:0 /y:0
 insertbmp /p:"line.bmp" /x:880 /y:0 /z:2660
 insertbmp /p:"line.bmp" /x:880 /y:160 /z:2660
 insertbmp /p:"line.bmp" /x:880 /y:320 /z:2660
-insertbmp /p:"gradient.bmp" /x:229 /y:624
-insertbmp /p:"line_a.bmp" /x:880 /y:480 /z:2660
+insertbmp /p:"gradient.bmp" /x:0 /y:624
+%tall%
 
 
 set /a hest=%random% %% 7 + 1
@@ -99,6 +99,7 @@ if "%hest1%" == "8" insertbmp /p:"hest1.bmp" /x:640 /y:0
 if "%hest1%" == "9" insertbmp /p:"hest1.bmp" /x:720 /y:0
 if "%hest1%" == "10" goto h1
 
+
 if "%hest2%" == "0" insertbmp /p:"hest2.bmp" /x:0 /y:80
 if "%hest2%" == "1" insertbmp /p:"hest2.bmp" /x:80 /y:80
 if "%hest2%" == "2" insertbmp /p:"hest2.bmp" /x:160 /y:80
@@ -111,6 +112,7 @@ if "%hest2%" == "8" insertbmp /p:"hest2.bmp" /x:640 /y:80
 if "%hest2%" == "9" insertbmp /p:"hest2.bmp" /x:720 /y:80
 if "%hest2%" == "10" goto h2
 
+
 if "%hest3%" == "0" insertbmp /p:"hest3.bmp" /x:0 /y:160
 if "%hest3%" == "1" insertbmp /p:"hest3.bmp" /x:80 /y:160
 if "%hest3%" == "2" insertbmp /p:"hest3.bmp" /x:160 /y:160
@@ -122,6 +124,7 @@ if "%hest3%" == "7" insertbmp /p:"hest3.bmp" /x:560 /y:160
 if "%hest3%" == "8" insertbmp /p:"hest3.bmp" /x:640 /y:160
 if "%hest3%" == "9" insertbmp /p:"hest3.bmp" /x:720 /y:160
 if "%hest3%" == "10" goto h3
+
 
 if "%hest4%" == "0" insertbmp /p:"hest4.bmp" /x:0 /y:240
 if "%hest4%" == "1" insertbmp /p:"hest4.bmp" /x:80 /y:240
@@ -147,6 +150,7 @@ if "%hest5%" == "8" insertbmp /p:"hest5.bmp" /x:640 /y:320
 if "%hest5%" == "9" insertbmp /p:"hest5.bmp" /x:720 /y:320
 if "%hest5%" == "10" goto h5
 
+
 if "%hest6%" == "0" insertbmp /p:"hest6.bmp" /x:0 /y:400
 if "%hest6%" == "1" insertbmp /p:"hest6.bmp" /x:80 /y:400
 if "%hest6%" == "2" insertbmp /p:"hest6.bmp" /x:160 /y:400
@@ -158,6 +162,7 @@ if "%hest6%" == "7" insertbmp /p:"hest6.bmp" /x:560 /y:400
 if "%hest6%" == "8" insertbmp /p:"hest6.bmp" /x:640 /y:400
 if "%hest6%" == "9" insertbmp /p:"hest6.bmp" /x:720 /y:400
 if "%hest6%" == "10" goto h6
+
 
 if "%hest7%" == "0" insertbmp /p:"hest7.bmp" /x:0 /y:480
 if "%hest7%" == "1" insertbmp /p:"hest7.bmp" /x:80 /y:480
@@ -171,12 +176,12 @@ if "%hest7%" == "8" insertbmp /p:"hest7.bmp" /x:640 /y:480
 if "%hest7%" == "9" insertbmp /p:"hest7.bmp" /x:720 /y:480
 if "%hest7%" == "10" goto h7
 
-Batbox /g 0 39 /c 0x1F /d "Du har valgt hest %v%"
+
 Timeout /t 2 /nobreak >nul
 goto kalkulator
 
 :h1
-color 0F
+color 07 & color 0F
 if "%v1%" == "true" echo Du vant 5000!
 if "%v1%" == "true" set /a penger+=5000
 if "%v1%" == "false" echo Du tapte
@@ -184,7 +189,7 @@ pause >nul
 goto meny
 
 :h2
-color 0F
+color 07 & color 0F
 if "%v2%" == "true" echo Du vant 5000!
 if "%v2%" == "true" set /a penger+=5000
 if "%v2%" == "false" echo Du tapte
@@ -192,7 +197,7 @@ pause >nul
 goto meny
 
 :h3
-color 0F
+color 07 & color 0F
 if "%v3%" == "true" echo Du vant 5000!
 if "%v3%" == "true" set /a penger+=5000
 if "%v3%" == "false" echo Du tapte
@@ -200,7 +205,7 @@ pause >nul
 goto meny
 
 :h4
-color 0F
+color 07 & color 0F
 if "%v4%" == "true" echo Du vant 5000!
 if "%v4%" == "true" set /a penger+=5000
 if "%v4%" == "false" echo Du tapte
@@ -208,7 +213,7 @@ pause >nul
 goto meny
 
 :h5
-color 0F
+color 07 & color 0F
 if "%v5%" == "true" echo Du vant 5000!
 if "%v5%" == "true" set /a penger+=5000
 if "%v5%" == "false" echo Du tapte
@@ -216,7 +221,7 @@ pause >nul
 goto meny
 
 :h6
-color 0F
+color 07 & color 0F
 if "%v6%" == "true" echo Du vant 5000!
 if "%v6%" == "true" set /a penger+=5000
 if "%v6%" == "false" echo Du tapte
@@ -224,7 +229,7 @@ pause >nul
 goto meny
 
 :h7
-color 0F
+color 07 & color 0F
 if "%v7%" == "true" echo Du vant 5000!
 if "%v7%" == "true" set /a penger+=500
 if "%v7%" == "false" echo Du tapte
